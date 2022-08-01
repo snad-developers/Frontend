556c5e835341221e2a1d1151322954b680e1848d
mounikaboinapally <mounika.boinapally12@gmail.com>
2022-08-01 15:05:51 +0530

    iserted console log

--- src/components/forgotpassword.vue
+++ src/components/forgotpassword.vue
@@ -1,64 +1,58 @@
 <template>
-  
-<div class = "main-container" style="margin-top:140px"> 
 
-  <div class="child-div1">
-  <h1><b>ANALYZE</b></h1>
-  <p>TODAY'S CHALLANGES</p>
-  
-  <h1><b>ENVISION</b></h1>
-  <P>TOMMOROW'S INNOVATION</P>
+  <div class="main-container" style="margin-top:140px">
 
-  </div>
-  
-   <div class="child-div2">
-   
-    
- 
-  <form  class="loginhere"  >
-    <div class="img">
-      <img alt="" src="../assets/snadicon.png" />
-   </div>
-    <h2><b>Forgot Password?</b> </h2>
-    <br><br>
-      <h6>Enter the User Id you have used during registration</h6>
-     <div class="forms">
-       <div class="forms1">
-      <label for="UserId"></label>
-      <input type="text" class="user" placeholder="Enter Your User Id" v-model="person.UserId"
-            :class="
+    <div class="child-div1">
+      <h1><b>ANALYZE</b></h1>
+      <p>TODAY'S CHALLANGES</p>
+
+      <h1><b>ENVISION</b></h1>
+      <P>TOMMOROW'S INNOVATION</P>
+
+    </div>
+
+    <div class="child-div2">
+
+
+
+      <form class="loginhere">
+        <div class="img">
+          <img alt="" src="../assets/snadicon.png" />
+        </div>
+        <h2><b>Forgot Password?</b> </h2>
+        <br><br>
+        <h6>Enter the User Id you have used during registration</h6>
+        <div class="forms">
+          <div class="forms1">
+            <label for="UserId"></label>
+            <input type="text" class="user" placeholder="Enter Your User Id" v-model="person.UserId" :class="
               v$.person.UserId.$error === true
                 ? 'text-fields-error'
                 : 'text-fields'
-            "
-            style="margin-top:5%" >
- <p
-            class="text-red-500 text-xs font-thin"
-            v-for="error of v$.person.UserId.$errors"
-            :key="error.$uid"
-          >
-            {{ error.$message }}
-          </p>
-      </div>
-  
-  
-     
-   <br>
-    <button  class= "button" @click.prevent="submit">Continue </button>
-    
-<p  class="text-red-500 text-xs font-thin" style="padding:6px;">{{message}}</p> 
-   
-   </div> 
-   
-      
-  </form> 
- 
-</div>
+            " style="margin-top:5%">
+            <p class="text-red-500 text-xs font-thin" v-for="error of v$.person.UserId.$errors" :key="error.$uid">
+              {{ error.$message }}
+            </p>
+          </div>
 
 
-</div>
 
-    
+          <br>
+          <button class="button" @click.prevent="submit">Continue </button>
+
+          <p class="text-red-500 text-xs font-thin" style="padding:6px;">{{ message }}</p>
+
+        </div>
+
+
+      </form>
+
+    </div>
+
+
+  </div>
+
+
 </template> 
 
 
@@ -68,21 +62,21 @@ import useVuelidate from "@vuelidate/core";
 import { required, helpers, email } from "@vuelidate/validators";
 import loginapi from '../services/loginapi';
 export default {
-     // eslint-disable-next-line vue/multi-word-component-names
-     name: 'forgotpassword',
-   
-      data() {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'forgotpassword',
+
+  data() {
     return {
       v$: useVuelidate(),
       person: {
         UserId: null,
-      
+
       },
-      message:"",
-   
+      message: "",
+
     };
   },
- 
+
 
   validations() {
     return {
@@ -92,113 +86,122 @@ export default {
           $autoDirty: true,
           email: helpers.withMessage("Please enter Valid User id", email)
         },
-      
+
       },
     };
   },
-   created() {
+  created() {
     this.GetloginDetails();
-        },
- methods: {
-   GetloginDetails(){
-                 if(localStorage.getItem('currentUser')){
-                 this.$router.push({name:"launchpage"});
-                 }
-        
+  },
+  methods: {
+    GetloginDetails() {
+      if (localStorage.getItem('currentUser')) {
+        this.$router.push({ name: "launchpage" });
+      }
+
     },
     submit() {
-       this.v$.$touch();
-       console.log(this.v$)
-       if(!this.v$.$invalid){
+      this.v$.$touch();
+      console.log(this.v$)
+      if (!this.v$.$invalid) {
         let sdata = {
-    "email":this.person.UserId
-}
- this.responsedata=loginapi.forgotpassword(sdata).then(response=>{
-console.log(response,"response data");
-if(response.data){
-  console.log("if condition")
- if(response.data.status == "success" && response.data.statuscode == 200){
-    this.$router.push({ name: 'securityquestions', params: { id: response.data.mailid }})
+          "email": this.person.UserId
+        }
+        this.responsedata = loginapi.forgotpassword(sdata).then(response => {
+          console.log(response, "response data");
+          if (response.data) {
+            console.log("if condition")
+            if (response.data.status == "success" && response.data.statuscode == 200) {
+              this.$router.push({ name: 'securityquestions', params: { id: response.data.mailid } })
 
- }
-  if(response.data.status == "failure" && response.data.statuscode == 201){
-  this.message=response.data.message
- }
+            }
+            if (response.data.status == "failure" && response.data.statuscode == 201) {
+              this.message = response.data.message
+              console.log("message")
+            }
 
-}
-  
-       })
+          }
 
-       }
- },
+        })
+
+      }
+    },
 
   },
-  
+
 }
 
 </script>
 
 
 <style>
-.font-thin{
+.font-thin {
   font-size: 14px;
   color: #c40b0b;
 }
-.main-container{
+
+.main-container {
   display: flex;
   flex-direction: row;
 }
-.child-div1{
+
+.child-div1 {
   width: 65%;
 }
-.child-div2{
+
+.child-div2 {
   width: 35%;
 }
 
-div{
+div {
   text-align: center;
 }
-.backbtn{
-    text-align: left;
+
+.backbtn {
+  text-align: left;
 }
 
-input[type=text]{
-    
-    padding: 12px 20px;
-    border: 1px solid;
-    border-radius: 20px;
-    text-align: center;
-     font-family: sans-serif;
-   
-}
-input[type=password]{
+input[type=text] {
 
-    padding: 12px 20px;
-    border: 1px solid;
-    border-radius: 20px;
-    text-align: center;
-     font-family: sans-serif;
+  padding: 12px 20px;
+  border: 1px solid;
+  border-radius: 20px;
+  text-align: center;
+  font-family: sans-serif;
+
 }
-select{
-    width: 28%;
-    padding: 12px 20px;
-    border: 1px solid;
-    border-radius: 20px;
-    text-align: center;
-     font-family: sans-serif;
+
+input[type=password] {
+
+  padding: 12px 20px;
+  border: 1px solid;
+  border-radius: 20px;
+  text-align: center;
+  font-family: sans-serif;
 }
-input[type=button]{
-    width: 10%; 
-    padding: 12px 20px;
-    border: 1px solid;
-    border-radius: 20px;
-    text-align: center;
-    background-color: rgb(119, 6, 6);
-    color: white;
-     font-family: sans-serif;
+
+select {
+  width: 28%;
+  padding: 12px 20px;
+  border: 1px solid;
+  border-radius: 20px;
+  text-align: center;
+  font-family: sans-serif;
 }
-.button{
-    
+
+input[type=button] {
+  width: 10%;
+  padding: 12px 20px;
+  border: 1px solid;
+  border-radius: 20px;
+  text-align: center;
+  background-color: rgb(119, 6, 6);
+  color: white;
+  font-family: sans-serif;
+}
+
+.button {
+
   width: 100%;
   margin-bottom: 10px;
   padding: 10px;
@@ -209,86 +212,92 @@ input[type=button]{
   transition: border-color 0.5s;
   font-family: sans-serif;
 }
-.button1{
-    
-    padding: 12px 20px;
-    border: 1px solid;
-    border-radius: 20px;
-    text-align: center;
-    background-color:  rgb(15, 6, 119);
-    color: white;
-     font-family: sans-serif;
-     transition: 0.5s;
+
+.button1 {
+
+  padding: 12px 20px;
+  border: 1px solid;
+  border-radius: 20px;
+  text-align: center;
+  background-color: rgb(15, 6, 119);
+  color: white;
+  font-family: sans-serif;
+  transition: 0.5s;
 }
 
- 
 
-.img{
+
+.img {
   text-align: left 2px;
-  margin-left:-10px ;
+  margin-left: -10px;
   margin-bottom: 10px;
 }
 
-.backbtn{
+.backbtn {
   align-items: baseline;
-   font-family: sans-serif;
+  font-family: sans-serif;
 }
+
 input[type="checkbox"] {
   -webkit-appearance: checkbox;
-     -moz-appearance: checkbox;
-          appearance: checkbox;
+  -moz-appearance: checkbox;
+  appearance: checkbox;
   display: inline-block;
   font-family: sans-serif;
- width: auto;
+  width: auto;
 }
-.FG{
-   text-justify: auto;
-   text-align: right;
+
+.FG {
+  text-justify: auto;
+  text-align: right;
 }
-body{
-  background:-webkit-linear-gradient(left #bb8c9a) ;
+
+body {
+  background: -webkit-linear-gradient(left #bb8c9a);
   margin: 15px;
   margin-top: 50px;
   position: relative;
-   font-family: sans-serif;
+  font-family: sans-serif;
 
-}  
+}
+
 .loginhere,
 .loginhere * {
   box-sizing: border-box;
   font-family: sans-serif;
-  margin:0px;
+  margin: 0px;
 
 
-  
-  
 
-  
+
+
+
 }
 
 
-.loginhere{
+.loginhere {
   height: 97%;
-   max-width: 400px;
-   /* max-height: 5%; */
-   margin: 10px auto;
-   border-radius: 15px;
-   overflow: hidden;
-   box-shadow: 0 0 15px rgba(0,0,0,0.15);  
-   
-   margin-bottom: 0%;
-   font-family: sans-serif;
-   background-color:white;
-   
+  max-width: 400px;
+  /* max-height: 5%; */
+  margin: 10px auto;
+  border-radius: 15px;
+  overflow: hidden;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.15);
+
+  margin-bottom: 0%;
+  font-family: sans-serif;
+  background-color: white;
+
 }
 
 
-.imgs{
+.imgs {
   display: block;
   max-width: 125px;
   margin: 0 auto;
 }
-.forms{
+
+.forms {
   padding: 30px;
 
 
@@ -298,13 +307,13 @@ body{
 
 
 
-.user::placeholder{
+.user::placeholder {
   color: #aaaaaa;
 
 }
 
-.regis{
-   box-sizing: border-box;
+.regis {
+  box-sizing: border-box;
   font-family: sans-serif;
   margin-right: 75%;
   background: #eeee;
@@ -312,26 +321,25 @@ body{
   border-radius: 25px;
 
 
-  
-  
+
+
 }
-.form{
+
+.form {
   background-color: #dddddd;
 }
 
 
-.main-div{
-      display: flex;
-    justify-content: space-around;
+.main-div {
+  display: flex;
+  justify-content: space-around;
 }
-p{
+
+p {
   margin: 0px;
 }
-.child-div1{
-  margin:0px !important
+
+.child-div1 {
+  margin: 0px !important
 }
-
-
-
-
 </style>
